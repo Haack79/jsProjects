086719790d0c1a258f6e2365cecb77ba67b34dc9
Brian Haack <haack79@hotmail.com>
2020-08-24 09:01:00 -0700

    finished light-dark, started animation

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -5,6 +5,8 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
 
 // DRY for setting dark and light
 function imageMode(color) {
@@ -41,13 +43,13 @@ function imageMode(color) {
 // } 
 // Set function to toglle light and dark modes
 function toggleDarkLightMode(isDark) {
-    isDark ? document.documentElement.setAttribute('data-theme', 'dark') : document.documentElement.setAttribute('data-theme', 'light')
+    isDark ? document.documentElement.setAttribute('data-theme', DARK_THEME) : document.documentElement.setAttribute('data-theme', LIGHT_THEME)
     nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)': 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    isDark ? imageMode('dark') : imageMode('light'); 
-    isDark ? localStorage.setItem('theme', 'dark') : localStorage.setItem('theme', 'light');
+    isDark ? imageMode(DARK_THEME) : imageMode(LIGHT_THEME); 
+    isDark ? localStorage.setItem('theme', DARK_THEME) : localStorage.setItem('theme', LIGHT_THEME);
 }
 // Switch Theme Dynamically
 function switchTheme(event) {
@@ -63,7 +65,7 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
+    if (currentTheme === DARK_THEME) {
         toggleSwitch.checked = true;
         toggleDarkLightMode(true);
     }
