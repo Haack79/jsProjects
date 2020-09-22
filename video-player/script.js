97bdc78e47665e59a6caa3a223007d200c79eff2
Brian Haack <haack79@hotmail.com>
2020-09-22 09:11:11 -0700

    initial play set up

--- video-player/script.js
+++ video-player/script.js
@@ -1,7 +1,22 @@
-
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('progress-range');
+const progressBar = document.querySelector('progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('volumen-range');
+const volumeBar = document.querySelector('volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullScreenBtn = document.querySelector('.fullscreen');
 // Play & Pause ----------------------------------- //
-
+ // first look at docs for it online. methods for vid play
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -18,4 +33,8 @@
 
 // Fullscreen ------------------------------- //
 
+
+// EVENT LISTENERS ----------------------------//
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay); 
 
