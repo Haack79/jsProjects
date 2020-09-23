4d829e38c10abf73b929ed13fb75bbfcca937ed3
Brian Haack <haack79@hotmail.com>
2020-09-23 09:12:32 -0700

    updated timing for play, pause, and ending a video

--- video-player/script.js
+++ video-player/script.js
@@ -10,17 +10,39 @@ const duration = document.querySelector('.time-duration');
 const fullScreenBtn = document.querySelector('.fullscreen');
 // Play & Pause ----------------------------------- //
  // first look at docs for it online. methods for vid play
+
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 function togglePlay() {
     if (video.paused) {
         video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
     } else {
         video.pause();
+        showPlayIcon();
     }
 }
-
+// on Video end, show play  button icon
+video.addEventListener('ended', showPlayIcon); 
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+//Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)}/`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
 
 
 // Volume Controls --------------------------- //
@@ -37,4 +59,5 @@ function togglePlay() {
 // EVENT LISTENERS ----------------------------//
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay); 
-
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
