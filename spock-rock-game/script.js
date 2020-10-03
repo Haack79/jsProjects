9cdaef10adb3c7be5f90c6dfb1d70531bb35fd30
Brian Haack <haack79@hotmail.com>
2020-10-03 13:00:36 -0700

    created ability to choose for computer and human player

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -26,16 +26,74 @@ const choices = {
   lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
-
+let playerScoreNumber = 0;
+let computerScoreNumber = 0; 
+let computerChoice = '';
 // Reset all 'selected' icons 
 function resetSelected() {
   allGameIcons.forEach((icon) => {
     icon.classList.remove('selected');
   })
 }
-//Passing player selection values 
-function select(playerChocie) {
+
+// Random computer choice
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = 'rock';
+  } else if (computerChoiceNumber  <= 0.4) {
+    computerChoice = 'paper';
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissors';
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard';
+  } else {
+    computerChoice = 'spock';
+  }
+}
+// Add 'selected' styling and computer choise
+function displayComputerChoice() {
+  // Add 'selected' styling & playerchoice
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+    default:
+      break;
+  }
+}
+// check result, increase score , update results
+function updateScore(playerChoice) {
+  
+}
+
+// Call functions to process turn
+function checkResult(playerChoice) {
   resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+  updateScore(playerChoice); 
+}
+//Passing player selection values 
+function select(playerChoice) {
+  checkReulst(playerChoice);
   // Add 'selected' styling & playerchoice
   switch (playerChoice) {
     case 'rock':
