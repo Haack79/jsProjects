c43a5cd363f720034057859304f43e10138c2dbb
Brian Haack <haack79@hotmail.com>
2020-10-20 09:46:09 -0700

    updated ability to add items and make text editable

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -100,6 +100,29 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Add to Column List, Reset Textbox
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM(); 
+}
+// Show add item input box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide item input box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
 // Allows arrays to reflect Drag and drop items
 function rebuildArrays() {
   backlogListArray = [];
